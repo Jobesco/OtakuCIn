265d54c7ed86c969052b493acac6436efd9c8d00
romildojuliano <rjal@cin.ufpe.br>
2022-10-23 19:11:55 -0300

    build(gerador.py): Tentando criar a tabela da Obra; é necessário referenciar fks da tabela de usuário BREAKING CHANGE:

--- gerador.py
+++ gerador.py
@@ -1,18 +1,22 @@
+import re
 import jinja2
 import numpy as np
-from numbers import Number
 from faker import Faker
-import re
+from numbers import Number
+from ranimegen.animegen import RandomAnime
 
 date_regex = "^[0-9]{4}-[0-9]{2}-[0-9]{2}"
 class Sql_table:
     attributes: dict
     table_name: str
+    sql_data : str
 
     def __init__(self, attributes=None, table_name=None) -> None:
         self.attributes = attributes
         self.table_name = table_name    
+        self.sql_data = str()
 
+    # Gera um campo
     def generate_fields(self):
         s = '('
         keys = self.attributes.keys()
@@ -21,21 +25,30 @@ class Sql_table:
         s += ')'
         return s
 
-    def generate_data(self, datasize, template, table_name=None):
+    # Gera uma lista de campos
+    def generate_data(self, datasize, data_model, template, table_name, verbose=False):
         if table_name == None: table_name = self.table_name
 
         for i in range(datasize):
             s = str()
-            items = Usuario.attributes.items()
-            for i, (key, item) in enumerate(items):
-                element = Usuario.attributes[key]()
+            items = data_model.items()
+            for _, value in items:
+                element = value()
+                # print(element)
                 if (isinstance(element, Number)): s += f'{element}'
                 elif (re.search(date_regex, str(element))): s += f"TO_DATE('{element}', 'YYYY-MM-DD')"
                 else: s += f"'{element}'"
 
                 if (i != len(items)-1): s += ', '
-                    
-            print(template.render(tabela = Usuario.table_name, campos = Usuario.generate_fields(), dados=s))
+            
+            sql_statement = template.render(tabela = table_name, campos = tuple(list(data_model.keys())), dados=s)
+            self.sql_data += sql_statement
+            if (verbose == True): print(sql_statement)
+    
+    # Gera um arquivo
+    def generate_sql_file(self, filename):
+        with open(filename, "r") as f:
+            f.write(self.sql_data)
         
 
 f = Faker('pt_BR')
@@ -43,6 +56,7 @@ f = Faker('pt_BR')
 sex = lambda: np.random.choice(['M', 'F'])
 num = lambda: np.random.randint(1,1000)
 tipo = lambda: np.random.choice(['Normal', 'Publicador'])
+# Usuário
 
 # ! modifique aqui os campos
 user_table = {
@@ -59,15 +73,40 @@ user_table = {
 table_name = "Usuario"
 populate = "INSERT INTO {{ tabela }}{{ campos }} VALUES ({{ dados }});\n"
 template = jinja2.Template(populate)
-Usuario = Sql_table(
-    attributes=user_table,
-    table_name='Usuario' # ! modifique aqui o nome da tabela
-)
 
+sql_generator = Sql_table()
 DATASIZE = 100
 
-Usuario.generate_data(DATASIZE, template, user_table, table_name)
+sql_generator.generate_data(
+    DATASIZE,       # <- Insira aqui a quantidade de dados a serem gerados
+    user_table,     # <- Insira aqui a tarefa dos usuários
+    template,       # <- Insira aqui o template carregado do jinja
+    table_name,     # <- Insira aqui o nome da tabela a ser gerada
+    verbose=True    # <- Insira aqui se a execução da tabela terá o print
+)
 
+# Gerador 
+
+
+# Obra
+async def random_anime_name():
+    gen = RandomAnime()
+    myinfo = await gen.suggestanime()
+    
+    return myinfo['data'][0]['attributes']['slug']
+
+
+obra_table = {
+    "id_obra": np.random([1, int(1e10000)]),
+    "nome_obra": random_anime_name,
+    "autor": None,
+    "nota": None,
+    "desenhista": None,
+    "diretor": None,
+    "eh_anime": None,
+    "eh_manga": None,
+    "cpf": None,
+}
 
 
 
